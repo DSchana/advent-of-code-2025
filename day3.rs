43cb04af252a82b5c0dc83a47736b9bc25327800
Dilpreet <dschana6@pm.me>
2026-03-21 15:16:57 -0400

    day3 finish

--- day3.rs
+++ day3.rs
@@ -1,35 +1,33 @@
 use std::fs;
 
-fn max_joltage(bb: &[u32], n_bat: u32) -> u32 {
-    // TODO: Loop through n battries finding the largest number
-    // from the previous index and keep a sum += m * 10^bat_pos
-
-
-    let (m_i, m) = bb[..bb.len() - 1].iter()
-        .enumerate()
-        .fold((0, &bb[0]), |(max_i, max_v), (i, v)| {
-            if v > max_v { (i, v) } else { (max_i, max_v) }
-        });
-
-    let l = bb[m_i + 1..].iter()
-        .max()
-        .unwrap();
-
-    m * 10 + l
+fn max_joltage(bb: &[u64], n_bat: u64) -> u64 {
+    let mut mi = 0;
+    let mut mj = 0;
+    for rb in (0..n_bat).rev() {
+        let end = bb.len() - rb as usize;
+        let (n_mi, n_mj) = bb[mi..end].iter()
+            .enumerate()
+            .fold((0, &bb[mi]), |(max_i, max_v), (i, v)| {
+                if v > max_v { (i, v) } else { (max_i, max_v) }
+            });
+        mi += n_mi + 1;
+        mj += n_mj * 10_u64.pow(rb as u32);
+    }
+    mj
 }
 
 fn main() {
     let input = fs::read_to_string("day3-input").expect("Failed to read");
-    let battery_bank: Vec<Vec<u32>> = input
+    let battery_bank: Vec<Vec<u64>> = input
         .lines()
         .map(|line| {
             line.chars()
-                .filter_map(|c| c.to_digit(10))
+                .filter_map(|c| c.to_digit(10).map(|d| d as u64))
                 .collect()
         })
         .collect();
 
-    let total: u32 = battery_bank
+    let total: u64 = battery_bank
         .iter()
         .map(|bb| max_joltage(&bb, 12))
         .sum();
