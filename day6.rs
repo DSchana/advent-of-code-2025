1fbaef936242f68f012e318f3e4da1a033e80650
Dilpreet <dschana6@pm.me>
2026-03-30 08:27:47 -0400

    day6 finish

--- day6.rs
+++ day6.rs
@@ -2,63 +2,94 @@ use std::fs;
 use std::time::Instant;
 
 macro_rules! time {
-  ($name:ident, $body:expr) => {{
-    let start = Instant::now();
-    let res = $body;
-    println!("{}: {:?} --> {}", stringify!($name), start.elapsed(), res);
-  }};
+    ($name:ident, $body:expr) => {{
+        let start = Instant::now();
+        let res = $body;
+        println!("{}: {:?} --> {}", stringify!($name), start.elapsed(), res);
+    }};
 }
 
 fn part1(num_arrays: &Vec<Vec<u64>>, ops: &Vec<&str>) -> u64 {
-  num_arrays.iter()
-    .enumerate()
-    .map(|(i, nums)| {
-      let op = ops[i];
-      nums.iter().fold(if op == "*" { 1 } else { 0 }, |t, n| {
-        if op == "*" { t * n } else { t + n }
-      })
-    })
-    .sum()
+    num_arrays.iter()
+        .enumerate()
+        .map(|(i, nums)| {
+            let op = ops[i];
+            nums.iter().fold(if op == "*" { 1 } else { 0 }, |t, n| {
+                if op == "*" { t * n } else { t + n }
+            })
+        })
+        .sum()
 }
 
 fn part2(lines: &Vec<String>) -> u64 {
+    let mut final_nums = vec![];
+    let mut skip_blank_col = false;
+    lines.last().unwrap().chars()
+        .enumerate()
+        .map(|(i, v)| {
+            if skip_blank_col {
+                skip_blank_col = false;
+                return 0;
+            }
+            let mut p = 1u64;
+            let num = lines[..lines.len() - 1].iter().rev()
+                .fold(0, |num, line| {
+                    let mut is_blank = false;
+                    let digit = line.chars().nth(i)
+                        .and_then(|c| c.to_digit(10))
+                        .unwrap_or_else(|| {
+                            is_blank = true;
+                            0
+                        }) as u64;
+                    let new_num = num + digit * p;
+                    if !is_blank {
+                        p *= 10;
+                    }
+                    new_num
+                });
+            final_nums.push(num);
 
-
-  lines.iter()
-    .enumerate()
-    .map(|(i, v)| {
-      let p = 1;
-      let mut res = 0u64;
-      match lines.last()[i] {
-        " " => lines[..len() - 1].iter()
-                .map(||)
-      }
-    })
-    .sum()
+            match v {
+                '+' => {
+                    let result = final_nums.iter().fold(0, |t, n| t + n);
+                    final_nums = vec![];
+                    skip_blank_col = true;
+                    result
+                },
+                '*' => {
+                    let result = final_nums.iter().fold(1, |t, n| t * n);
+                    final_nums = vec![];
+                    skip_blank_col = true;
+                    result
+                },
+                _ => 0,
+            }
+        })
+        .sum()
 }
 
 fn main() {
-  let input = fs::read_to_string("day6-input").expect("Failed to read");
-  let lines: Vec<&str> = input.lines().collect();
-  let ops: Vec<&str> = lines.last().copied().unwrap_or_default().split_whitespace().collect();
-  let num_lines: Vec<&str> = lines[..lines.len() - 1].to_vec();
-  let nums: Vec<Vec<u64>> = (0..ops.len())
-    .map(|i| {
-      num_lines
+    let input = fs::read_to_string("day6-input").expect("Failed to read");
+    let lines: Vec<&str> = input.lines().collect();
+    let ops: Vec<&str> = lines.last().copied().unwrap_or_default().split_whitespace().collect();
+    let num_lines: Vec<&str> = lines[..lines.len() - 1].to_vec();
+    let nums: Vec<Vec<u64>> = (0..ops.len())
+        .map(|i| {
+            num_lines
+                .iter()
+                .map(|row| {
+                    row.split_whitespace().nth(i)
+                        .expect("Col idx out of bounds")
+                        .parse::<u64>()
+                        .expect("Failed to parse num")
+                }).collect()
+        }).collect();
+
+    time!(part1, part1(&nums, &ops));
+
+    let rev_lines: Vec<String> = lines
         .iter()
-        .map(|row| {
-          row.split_whitespace().nth(i)
-            .expect("Col idx out of bounds")
-            .parse::<u64>()
-            .expect("Failed to parse num")
-        }).collect()
-    }).collect();
-
-  time!(part1, part1(&nums, &ops));
-
-  let rev_lines: Vec<String> = lines
-    .iter()
-    .map(|s| s.chars().rev().collect())
-    .collect();
-  time!(part2, part2(&rev_lines));
+        .map(|s| s.chars().rev().collect())
+        .collect();
+    time!(part2, part2(&rev_lines));
 }
